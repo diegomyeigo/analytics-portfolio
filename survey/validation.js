5d7ea860c5776de9a400851a0fb5aaf74375cb71
Jose Perez <jdiegoperez001@gmail.com>
2026-08-04 21:46:19 -0500

    Added js validation

---
+++ survey/validation.js
@@ -0,0 +1,41 @@
+(function () {
+    const form = document.getElementById("finance_survey");
+    const subButton = document.getElementById("submit");
+
+    function checkFields() {
+        subButton.disabled = !form.checkValidity();
+    }
+
+    form.addEventListener("input", checkFields);
+    form.addEventListener("change", checkFields);
+
+    checkFields();
+
+}());
+
+function raiseRangeError(id, flag) {
+    const input = document.getElementById(id);
+    const error = document.getElementById(flag);
+
+    input.addEventListener("input", () => {
+        const value = Number(input.value);
+        const min = Number(input.min);
+        const max = Number(input.max);
+
+        if ((input.value === "") || (value > min & value < max)) {
+            error.classList.replace("error", "pass");
+            error.textContent = "";
+            return;
+        } else if (value < min || value > max) {
+            error.textContent = `Value must be between ${min} and ${max}`;
+            error.classList.replace("pass", "error");
+            return;
+        }
+
+})};
+
+
+raiseRangeError("income", "incomeError");
+raiseRangeError("rent", "rentError");
+raiseRangeError("savings", "savingsError");
+raiseRangeError("emergency", "emergencyError");
